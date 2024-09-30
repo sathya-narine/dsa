2d877fedfc99ab390415e20cbcf7e8df169aafb0
sathya-narine <sathyanarine21@gmail.com>
2024-09-30 12:17:47 -0400

    remove duplicate

---
+++ remove_duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = sorted(set(nums))
+        print(nums)
+        return len(nums)
